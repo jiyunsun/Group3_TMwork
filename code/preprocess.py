a890d8376cdf107dd65a93f481d4a3e788262bb5
unknown <tesselwisman@gmail.com>
2022-01-20 16:38:32 +0100

    Included all features

--- code/preprocess.py
+++ code/preprocess.py
@@ -4,36 +4,73 @@ import nltk
 import spacy
 import string
 from utils import *
-from feature_extraction import *
 
-
-
-def add_feature_columns(conll_file, outputfilename, lexicon):
-    '''
-    Check which annotations need to be converted for the output to match and convert them
-
-    :param conll_object: structured object with conll annotations
-    :param annotation_identifier: indicator of how to find the annotations in the object (e.g. key of dictionary, index of list)
-    '''
+def remove_blanks(conll_file, outputfilename):
     conll_object = read_in_conll_file(conll_file)
-    with open(outputfilename, 'w') as outputcsv:
+    with open(outputfilename, 'w', newline='') as outputcsv:
         csvwriter = csv.writer(outputcsv, delimiter='\t')
-        header = ['doc_id', 'sentence_id', 'token_id', 'token', 'pos', 'start_ngrams', 'end_ngrams', 'sbs_count', 'negcue']
-        csvwriter.writerow(header)
         for row in conll_object:
             if len(row) <= 0:
                 continue
-            token = get_token(row)
-            if not token:
-                continue
-            pos = nltk.pos_tag([token])[0][1]
-            start_ngrams, end_ngrams, sbs_count = create_ngram_features(token, lexicon, n=5)
-            row[3] = token
-            row.insert(4, pos)
-            row.insert(5, start_ngrams)
-            row.insert(6, end_ngrams)
-            row.insert(7, sbs_count)
 
+            csvwriter.writerow(row)
+
+def add_feature_columns(conll_file, outputfilename, lexicon, tokenlist):
+    '''
+    Preprocess the file by converting the content and adding feature columns.
+    Converts:
+        -tokens to lowercase
+        -labels from BIO-NEG(cues) to binary format (1 if NegCue, 0 otherwise)
+    Adds:
+        - POS tag
+        -
+
+    :param conll_object: structured object with conll annotations
+    :param
+    '''
+    conll_object = read_in_conll_file(conll_file)
+    with open(outputfilename, 'w', newline='') as outputcsv:
+        csvwriter = csv.writer(outputcsv, delimiter='\t')
+        header = ['doc_id', 'sentence_id', 'token_id', 'token', 'lemma', 'prev', 'next','pos', 'punct', 'suffix', 'infix', 'prev_tok_cue', 'sbs_count', 'match_one', 'match_multi', 'negcuelabel']
+        csvwriter.writerow(header)
+        prev_token_label = 0
+        prev_token = ''
+        doc = tokenlist2doc(tokenlist)
+        for d in doc:
+            print(d, end='-')
+        for i, (row, token) in enumerate(zip(conll_object, doc)):
+            #print(token, 'xxx', row)
+            lemma = get_lemma(token)
+            pos = get_PoS(token)
+            ends_with_suffix = has_neg_suff(token.text)
+            infix = has_negation_infix(token.text)
+            punctuation = is_punctuation(token.text)
+            try:
+                next_token = get_lemma(doc[i + 1])
+            except IndexError:
+                next_token = ''
+            start_ngrams, end_ngrams, sbs_count = create_ngram_features(token.text, lexicon, n=5)
+            matches_one = matches_oneword_negexpr(token.text)
+            matches_multi = matches_multiword_negexpr(token.text, i, doc)
+            #row[3] = token.text
+            row.insert(4, lemma)
+            row.insert(5, prev_token)
+            row.insert(6, next_token)
+            row.insert(7, pos)
+            row.insert(8, punctuation)
+            row.insert(9, ends_with_suffix)
+            row.insert(10, infix)
+            row.insert(11, prev_token_label)
+            #row.insert(6, start_ngrams)
+            #row.insert(7, end_ngrams)
+            row.insert(12, sbs_count)
+            row.insert(13, matches_one)
+            row.insert(14, matches_multi)
+
+            label = get_negcue_label(row, -1)
+            row[-1] = label
+            prev_token_label = label
+            prev_token = token
             csvwriter.writerow(row)
 
 def main(args=None):
@@ -43,7 +80,10 @@ def main(args=None):
     conll = read_in_conll_file(path)
     tokenlist = list_of_tokens(conll)
     lexicon =  generate_lexicon(tokenlist)
-    add_feature_columns(path, r'C:\Users\Tessel Wisman\Documents\TextMining\AppliedTMMethods\SEM-2012-SharedTask-CD-SCO-simple.v2\SEM-2012-SharedTask-CD-SCO-training-simple.v2-preprocessed.txt', lexicon)
+    no_blanks_path = r'C:\Users\Tessel Wisman\Documents\TextMining\AppliedTMMethods\SEM-2012-SharedTask-CD-SCO-simple.v2\SEM-2012-SharedTask-CD-SCO-training-simple.v2_rem_nl.txt'
+    remove_blanks(path, no_blanks_path)
+    out_path = r'C:\Users\Tessel Wisman\Documents\TextMining\AppliedTMMethods\SEM-2012-SharedTask-CD-SCO-simple.v2\SEM-2012-SharedTask-CD-SCO-training-simple.v2-preprocessed.txt'
+    add_feature_columns(no_blanks_path, out_path, lexicon, tokenlist)
     #print(tokenlist)
 
 args = ['x', r'C:\Users\Tessel Wisman\Documents\TextMining\AppliedTMMethods\SEM-2012-SharedTask-CD-SCO-simple.v2\SEM-2012-SharedTask-CD-SCO-training-simple.v2.txt']
--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,16 @@
 import csv
 import string
 from collections import Counter
+from nltk import ngrams
+import spacy
+from spacy.tokenizer import Tokenizer
+import re
+
+def custom_tokenizer(nlp):
+    # this code was taken from https://stackoverflow.com/questions/57945902/force-spacy-not-to-parse-punctuation
+    prefix_re = re.compile('''^\$[a-zA-Z0-9]''')
+    return Tokenizer(nlp.vocab, prefix_search=prefix_re.search)
+
 
 def read_in_conll_file(conll_file, delimiter='\t'):
     '''
@@ -28,5 +38,125 @@ def get_token(row):
     #token_nopunct = token.translate(str.maketrans(dict.fromkeys(string.punctuation)))
     return token
 
-def get_negcue_label(row):
-    return row[4]
+def get_negcue_label(row, id):
+    return row[id][2:]
+
+def tokenlist2doc(token_list):
+    nlp = spacy.load('en_core_web_sm')
+    nlp.tokenizer = custom_tokenizer(nlp)
+    str = ' '
+    doc = str.join(token_list)
+    return nlp(doc)
+
+
+def get_lemma(spacy_token):
+    return spacy_token.lemma_
+
+def get_PoS(spacy_token):
+    return spacy_token.tag_
+
+
+def has_neg_suff(token):
+    if token.endswith('less'):
+        return 1
+    return 0
+
+def has_negation_infix(token):
+    #CONTAINS NEGATION INFIX:
+    if token.endswith("ness"): #instances like "carelessness"
+        cut_token_1 = token[:-4]
+        if cut_token_1.endswith("less"):
+            return 1
+    elif token.endswith("ly"): #instances like "restlessly"
+        cut_token_2 = token[:-2]
+        if cut_token_2.endswith("less"):
+            return 1
+    return 0
+
+def matches_oneword_negexpr(token):
+    negations = ["no", "not", "nor", "neither", "non", "without", "never", "cannot", "n't", "none", "nothing", "nobody",
+                        "nowhere"]
+    if token in negations:
+        return 1
+    return 0
+
+def matches_multiword_negexpr(token, i, doc):
+    multi_negations = ["no longer", "by no means", "not for the world", "on the contrary", "rather than",
+                       "nothing at all", "no more"]
+    three_prev = []
+    three_next = []
+    for j in range(1, 4):
+        try:
+            three_prev.append(doc[i+j].text)
+        except IndexError:
+            three_prev.append("")
+        try:
+            three_next.append(doc[i-j].text)
+        except IndexError:
+            three_next.append("")
+    #print(three_prev, three_next)
+    bigram = token + " " + three_prev[0]#create bi-grams and check if they match a multi_negation
+    reverse_bigram = three_next[0] + " " + token
+    trigram = three_prev[0] + " " + token + " " + three_next[0]
+    left_trigram = three_prev[1]+ " " + three_prev[0] + " " + token
+    right_trigram = token + " " + three_next[0] + " " + three_next[1]
+    left_fourgram = three_prev[2] + " " +three_prev[1] + " " + three_prev[0] + " " + token
+    left_center_fourgram = three_prev[1] + " " + three_prev[1] + " " + token + three_next[0]
+    right_center_fourgram = three_prev[0]+ " " + token + " " + three_next[0] + " " + three_next[1]
+    right_fourgram =   token + " " + three_next[0] + " " + three_next[1] + " " + three_next[2]
+
+    n_grams = [bigram, reverse_bigram, trigram, left_trigram, right_trigram, left_fourgram, left_center_fourgram, right_center_fourgram, right_fourgram]
+
+    if any(expr in multi_negations for expr in n_grams):
+        return 1
+    else:
+        return 0
+
+def is_punctuation(token):
+    if token in string.punctuation:
+        return 1
+    return 0
+
+def generate_lexicon(tokenlist, n=5):
+    lexicon = Counter()
+    lexicon.update(tokenlist)
+    for token in tokenlist:
+        if len(token) < n:
+            n_new = len(token)
+        else:
+            n_new = n
+        ngrams = generate_ngrams(token, n_new)[0] # start ngram
+        lexicon.update(ngrams)
+    return lexicon
+
+def generate_ngrams(token, n):
+    start_igrams = []
+    end_igrams = []
+    #print(token)
+    for i in reversed(range(1, n+1)):
+        #print(i)
+        igrams = [''.join(gram) for gram in ngrams(token, i)]
+        start_igrams.append(igrams[0])
+        end_igrams.append(igrams[-1])
+
+        #print('test', igrams[0])
+    #print(start_igrams, end_igrams)
+    return start_igrams, end_igrams
+
+def create_ngram_features(token, lexicon, n=5):
+    neg_prefixed = ['un', 'in', 'im', 'il', 'dis', 'ir', 'non']
+    affixless_token = ''
+    for prefix in neg_prefixed:
+        if token.startswith(prefix):
+            affixless_token = token[len(prefix):]
+    if token.endswith('less'):
+        affixless_token = token[:-len('less')]
+    if len(affixless_token) > 0:
+        if len(affixless_token) < n:
+            n = len(affixless_token)
+        start_ngrams, end_ngrams = generate_ngrams(affixless_token, n)
+        return start_ngrams, end_ngrams, lexicon[start_ngrams[0]] # first (longest start ngram of affixless token is the match we search for)
+    if len(token) < n:
+        n = len(token)
+    start_ngrams, end_ngrams = generate_ngrams(token, n)
+    return start_ngrams, end_ngrams, 0
